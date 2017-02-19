07c6f6ae410018ddb431b8a88e6873bd73f20906
Gamaliel Espinoza <gamaliel.espinoza@gmail.com>
2017-02-19 10:27:48 -0700

    Porcelain

--- joom2wp.php
+++ joom2wp.php
@@ -7,106 +7,104 @@ use WP_CLI\Utils;
 
 class MigrateCommand extends \WP_CLI_Command {
 
-	public function migrate($args=[], $assoc_args=[], $verbose=true) {
+    public function migrate($args=[], $assoc_args=[], $verbose=true) {
 
-		global $wpdb;
-		$wpdb->show_errors();
+        global $wpdb;
+        $wpdb->show_errors();
 
-		$allArgs = wp_parse_args(
-			$assoc_args,
-			[
-				'db_host'  => 'localhost',
-				'db_user'  => 'root',
-				'post_type'=> 'post',
-				'taxonomy' => 'category',
-				'table_prefix' => 'jo_'
-			]
-		);
+        $allArgs = wp_parse_args(
+            $assoc_args,
+            [
+                'db_host'  => 'localhost',
+                'db_user'  => 'root',
+                'post_type'=> 'post',
+                'taxonomy' => 'category',
+                'table_prefix' => 'jo_'
+            ]
+        );
 
-		$postType = $allArgs['post_type'];
-		$tablePrefix = $allArgs['table_prefix'];
-		$joomlaURL = $allArgs['joomla_url'];
-		$taxonomy = $allArgs['taxonomy'];
+        $postType = $allArgs['post_type'];
+        $tablePrefix = $allArgs['table_prefix'];
+        $joomlaURL = $allArgs['joomla_url'];
+        $taxonomy = $allArgs['taxonomy'];
 
-		$mysql = mysqli_connect(
-			$allArgs['db_host'],
-			$allArgs['db_user'],
-			$allArgs['db_pwd'],
-			$allArgs['db_name']
-		);
+        $mysql = mysqli_connect(
+            $allArgs['db_host'],
+            $allArgs['db_user'],
+            $allArgs['db_pwd'],
+            $allArgs['db_name']
+        );
 
-		$categories = get_categories([
-			'taxonomy' => $taxonomy
-		]);
+        $categories = get_categories([
+            'taxonomy' => $taxonomy
+        ]);
 
-		// WORDPRESS CATEGORIES
+        // WORDPRESS CATEGORIES
 
-		$wp_cats = [];
+        $wp_cats = [];
 
-		foreach ($categories as $cat) {
-			$wp_cats[$cat->term_id] = $cat;
-		}
+        foreach ($categories as $cat) {
+            $wp_cats[$cat->term_id] = $cat;
+        }
 
-		// JOOMLA CATEGORIES
-		$cats = [];
+        // JOOMLA CATEGORIES
+        $cats = [];
 
-		$result = $mysql->query("
-			select id, alias, name
-			from {$tablePrefix}k2_categories
-		");
+        $result = $mysql->query("
+            select id, alias, name
+            from {$tablePrefix}k2_categories
+        ");
 
-		while ($row = $result->fetch_object()) {
-			$cats[$row->id] = $row;
-		}
+        while ($row = $result->fetch_object()) {
+            $cats[$row->id] = $row;
+        }
 
-		// JOOMLA K2
-		$result = $mysql->query("
-	        select i.id, i.title, i.alias, 
-	        	   i.`fulltext`, i.introtext, 
-	        	   i.catid, c.alias, i.created,
-	        	   c.name as catname
-	        from {$tablePrefix}k2_items as i 
-	        inner join {$tablePrefix}k2_categories as c 
-	            on i.catid = c.id 
-		");
+        // JOOMLA K2
+        $result = $mysql->query("
+            select i.id, i.title, i.alias, 
+                   i.`fulltext`, i.introtext, 
+                   i.catid, c.alias, i.created,
+                   c.name as catname
+            from {$tablePrefix}k2_items as i 
+            inner join {$tablePrefix}k2_categories as c 
+                on i.catid = c.id 
+        ");
 
-		$mediaUtil = new \Media_Command;
+        $mediaUtil = new \Media_Command;
 
-		while ($row = $result->fetch_object()) {
-			$catID = wp_create_category($row->catname);
+        while ($row = $result->fetch_object()) {
+            $catID = wp_create_category($row->catname);
 
-			$postID = wp_insert_post([
-				'post_title'   => utf8_encode($row->title),
-				'post_content' => utf8_encode($row->fulltext),
-				'post_excerpt' => utf8_encode($row->introtext),
-				'post_type'    => $postType,
-				'post_name'    => utf8_encode(substr($row->alias, 0, 200)),
-				'post_date'    => $row->created,
-				'post_category'=> [$catID],
-				'post_status'  => 'publish'
-			]);
+            $postID = wp_insert_post([
+                'post_title'   => utf8_encode($row->title),
+                'post_content' => utf8_encode($row->fulltext),
+                'post_excerpt' => utf8_encode($row->introtext),
+                'post_type'    => $postType,
+                'post_name'    => utf8_encode(substr($row->alias, 0, 200)),
+                'post_date'    => $row->created,
+                'post_category'=> [$catID],
+                'post_status'  => 'publish'
+            ]);
 
-			if ($postID) {
-				WP_CLI::log("Imported post {$postID}");
+            if ($postID) {
+                WP_CLI::log("Imported post {$postID}");
 
-				$md5ID = md5("Image" . $row->id);
-				$imagenURL = "{$joomlaURL}/media/k2/items/cache/{$md5ID}_XL.jpg";
+                $md5ID = md5("Image" . $row->id);
+                $imagenURL = "{$joomlaURL}/media/k2/items/cache/{$md5ID}_XL.jpg";
 
-				try {
-					$mediaUtil->import([$imagenURL], [
-						'post_id' => $postID,
-						'featured_image' => true,
-						'porcelain'
-					]);
-				} catch (Exception $ex) {
-					WP_CLI::warning("Couldn't import {$postID} media.");
-				}
-				
-			} else {
-				WP_CLI::log("Couldn't import post.");
-			}
-		}
-	}
+                //WP_CLI::runcommand('wp media import ')
+
+                $mediaUtil->import([$imagenURL], [
+                    'post_id' => $postID,
+                    'featured_image' => true,
+                    'porcelain' => true
+                ]);
+            
+            } else {
+                WP_CLI::log("Couldn't import post.");
+            }
+        }
+    }
 
 }
 
