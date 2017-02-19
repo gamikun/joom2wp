5f92801d1d4fd7d8ec4ecee3622486c496652f48
Gamaliel Espinoza <gamaliel.espinoza@gmail.com>
2017-02-19 00:54:31 -0700

    More testing

--- joom2wp.php
+++ joom2wp.php
@@ -7,7 +7,32 @@ use WP_CLI;
 class MigrateCommand extends \WP_CLI_Command {
 
 	public function migrate($args=[], $assoc_args=[], $verbose=true) {
-		echo 'hola mundo';
+		$this->process_args(
+			array(
+				0 => '', // .json map file
+			),
+			$args,
+			array(
+				'db_host'  => 'localhost',
+				'db_user'  => 'root',
+				'post_type'=> 'post',
+				'taxonomy' => 'category'
+			),
+			$assoc_args
+		);
+
+		$mysql = mysqli_connect(
+			$assoc_args['db_host'],
+			$assoc_args['db_user'],
+			$assoc_args['db_pwd'],
+			$assoc_args['db_name']
+		);
+
+		$categories = get_categories([
+			'taxonomy' => $assoc_args['taxonomy']
+		]);
+
+		var_dump($categories);
 	}
 
 }
