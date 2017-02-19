cdc134fc98f21ea6877e2e5abc2fdb1a626943e9
Gamaliel Espinoza <gamaliel.espinoza@gmail.com>
2017-02-18 23:28:04 -0700

    Al fin

--- jo2wp.py
+++ jo2wp.py
@@ -136,7 +136,7 @@ if not args.do_revert:
 
         post_id = subprocess.check_output([
             'wp', 'post', 'create',
-            '--post_title={}'.format(title.decode('latin1')),
+            '--post_title={}'.format(title),
             #'--post_content={}'.format(content),
             #'--post_excerpt={}'.format(excerpt),
             #'--post_type={}'.format(args.post_type),
